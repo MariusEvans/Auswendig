8758897aa5b0cd07a3441b72f0527ae32d3d6bdf
Marius Evans <38615325+MariusEvans@users.noreply.github.com>
2018-10-13 20:09:10 +0100

    Allow for searching for all cards containing an entered tag
    Show all flashcards in all sets which contain an entered tag

---
+++ auswendig/SearchTag.java
@@ -0,0 +1,84 @@
+/* Copyright (C) Septemeber 2018, Marius Evans, All Rights Reserved
+ * This file is part of project Auswendig
+ * Reproduction, sale and commerical distribution of this material is strictly forbidden unless prior written permission is obtained from Marius Evans
+ * Written by Marius Evans (mariusevans@outlook.com)
+ */
+
+package auswendig;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchTag 
+{   
+    public File[] listOfFiles;
+    public String[] listOfFileNames = {"","","","","","","","","","",""}; //10 sets supported
+    //public String[] listOfResults = {"","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
+    List<String> listOfResults = new ArrayList<>();
+    
+    public void readFileNames()
+    {
+        File folder = new File("C:\\Users\\Marius Evans\\Documents\\NetBeansProjects\\Auswendig\\src\\Sets\\");
+		
+	if(folder.isDirectory())
+        {
+            if(folder.list().length>0)
+            {
+		System.out.println("Directory is not empty!");
+                File[] listOfFiles = folder.listFiles();
+
+                for (int i = 0; i < listOfFiles.length; i++) 
+                {
+                  if (listOfFiles[i].isFile()) 
+                  {
+                    System.out.println("File " + listOfFiles[i].getName());
+                    listOfFileNames[i] = listOfFiles[i].getName();
+                  } 
+                }
+            }
+            else
+            {
+                System.out.println("Directory is empty!");		
+            }	
+	}
+        else
+        {	
+            System.out.println("This is not a directory");	
+	}
+    }
+    
+    public void readFiles(String tag)
+    {
+        int amountOfFiles = listOfFileNames.length;
+        
+        for(int z=0; z<amountOfFiles; z++)
+        {
+            try
+            {
+                Path pathSelectedItem = Paths.get("C:\\Users\\Marius Evans\\Documents\\NetBeansProjects\\Auswendig\\src\\Sets\\"+listOfFileNames[z]);
+                List<String> linesSelectedItem = Files.readAllLines(pathSelectedItem, StandardCharsets.UTF_8);
+                int amountOfLines = linesSelectedItem.size();
+                for(int b=0; b<amountOfLines; b++)
+                {
+                    String line = linesSelectedItem.get(b);
+                    if(line.contains(tag))
+                    {
+                        listOfResults.add("Set: "+listOfFileNames[z]+" | "+line);
+                    }
+                }
+                
+            }
+            catch(Exception exc)
+            {
+                System.out.println("Error reading files.");
+                System.out.println(exc);
+                exc.printStackTrace();
+            }
+        }
+    }
+}
--- auswendig/SearchTagGUI.java
+++ auswendig/SearchTagGUI.java
@@ -6,6 +6,7 @@
 
 package auswendig;
 
+import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 
@@ -13,13 +14,12 @@ public class SearchTagGUI extends javax.swing.JFrame
 {
     //OBJECTS
     DeleteSet deleteSet = new DeleteSet();
+    SearchTag searchTag = new SearchTag();
     
     public SearchTagGUI() 
     {
         System.out.println("Running SearchTermGUI.");
         initComponents();
-        deleteSet.readFileNames();
-        String[] listOfFileNames1 = deleteSet.listOfFileNames;
     }
 
     @SuppressWarnings("unchecked")
@@ -27,7 +27,7 @@ public class SearchTagGUI extends javax.swing.JFrame
     private void initComponents() {
 
         STYLEPANEL = new javax.swing.JPanel();
-        tfTerm = new javax.swing.JTextField();
+        tfTag = new javax.swing.JTextField();
         btnSearch = new javax.swing.JButton();
         cbxResults = new javax.swing.JComboBox<>();
         lblTerm = new javax.swing.JLabel();
@@ -42,6 +42,10 @@ public class SearchTagGUI extends javax.swing.JFrame
         miOpenSet = new javax.swing.JMenuItem();
         miCreateSet = new javax.swing.JMenuItem();
         miEditSet = new javax.swing.JMenuItem();
+        menuOther = new javax.swing.JMenu();
+        miMergeFolders = new javax.swing.JMenuItem();
+        miMergeSets = new javax.swing.JMenuItem();
+        miSettings = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Auswendig");
@@ -57,7 +61,6 @@ public class SearchTagGUI extends javax.swing.JFrame
         });
 
         cbxResults.setMaximumRowCount(100);
-        cbxResults.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cbxResults.setEnabled(false);
 
         lblTerm.setText("Tag:");
@@ -68,26 +71,27 @@ public class SearchTagGUI extends javax.swing.JFrame
         STYLEPANEL.setLayout(STYLEPANELLayout);
         STYLEPANELLayout.setHorizontalGroup(
             STYLEPANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, STYLEPANELLayout.createSequentialGroup()
-                .addContainerGap(41, Short.MAX_VALUE)
+            .addGroup(STYLEPANELLayout.createSequentialGroup()
+                .addGap(33, 33, 33)
                 .addGroup(STYLEPANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblTerm, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(lblResults, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(STYLEPANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(cbxResults, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(STYLEPANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(STYLEPANELLayout.createSequentialGroup()
-                        .addComponent(tfTerm, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(tfTag, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnSearch)))
-                .addGap(28, 28, 28))
+                        .addComponent(btnSearch)
+                        .addGap(0, 248, Short.MAX_VALUE))
+                    .addComponent(cbxResults, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         STYLEPANELLayout.setVerticalGroup(
             STYLEPANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(STYLEPANELLayout.createSequentialGroup()
                 .addGap(40, 40, 40)
                 .addGroup(STYLEPANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(tfTerm, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(tfTag, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblTerm))
                 .addGap(18, 18, 18)
@@ -172,13 +176,45 @@ public class SearchTagGUI extends javax.swing.JFrame
 
         MenuBar.add(menuSets);
 
+        menuOther.setText("Other");
+        menuOther.setFont(new java.awt.Font("Yu Gothic UI Semibold", 1, 12)); // NOI18N
+
+        miMergeFolders.setIcon(new javax.swing.ImageIcon(getClass().getResource("/auswendig/res/join-icon.png"))); // NOI18N
+        miMergeFolders.setText("Merge Folders");
+        miMergeFolders.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miMergeFoldersActionPerformed(evt);
+            }
+        });
+        menuOther.add(miMergeFolders);
+
+        miMergeSets.setIcon(new javax.swing.ImageIcon(getClass().getResource("/auswendig/res/join-icon.png"))); // NOI18N
+        miMergeSets.setText("Merge Sets");
+        miMergeSets.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miMergeSetsActionPerformed(evt);
+            }
+        });
+        menuOther.add(miMergeSets);
+
+        miSettings.setIcon(new javax.swing.ImageIcon(getClass().getResource("/auswendig/res/settings-icon.png"))); // NOI18N
+        miSettings.setText("Settings");
+        miSettings.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                miSettingsActionPerformed(evt);
+            }
+        });
+        menuOther.add(miSettings);
+
+        MenuBar.add(menuOther);
+
         setJMenuBar(MenuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(STYLEPANEL, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(STYLEPANEL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -233,7 +269,29 @@ public class SearchTagGUI extends javax.swing.JFrame
     private void btnSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSearchActionPerformed
         System.out.println("btnSearch pressed");
         cbxResults.setEnabled(true);
+        cbxResults.removeAllItems();
+        
+        String tag = tfTag.getText();
+        searchTag.readFileNames();
+        searchTag.readFiles(tag);
+        List<String> listOfResults1 = searchTag.listOfResults;
+        for(int i = 0; i<listOfResults1.size(); i++)
+        {
+            cbxResults.addItem(""+listOfResults1.get(i));
+        }
     }//GEN-LAST:event_btnSearchActionPerformed
+
+    private void miMergeFoldersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miMergeFoldersActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_miMergeFoldersActionPerformed
+
+    private void miMergeSetsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miMergeSetsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_miMergeSetsActionPerformed
+
+    private void miSettingsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_miSettingsActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_miSettingsActionPerformed
 
     /**
      * @param args the command line arguments
@@ -266,14 +324,18 @@ public class SearchTagGUI extends javax.swing.JFrame
     private javax.swing.JLabel lblResults;
     private javax.swing.JLabel lblTerm;
     private javax.swing.JMenu menuFolders;
+    private javax.swing.JMenu menuOther;
     private javax.swing.JMenu menuSets;
     private javax.swing.JMenuItem miCreateFolder;
     private javax.swing.JMenuItem miCreateSet;
     private javax.swing.JMenuItem miDeleteSet;
     private javax.swing.JMenuItem miEditFolder;
     private javax.swing.JMenuItem miEditSet;
+    private javax.swing.JMenuItem miMergeFolders;
+    private javax.swing.JMenuItem miMergeSets;
     private javax.swing.JMenuItem miOpenFolder;
     private javax.swing.JMenuItem miOpenSet;
-    private javax.swing.JTextField tfTerm;
+    private javax.swing.JMenuItem miSettings;
+    private javax.swing.JTextField tfTag;
     // End of variables declaration//GEN-END:variables
 }
